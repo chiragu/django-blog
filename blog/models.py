91617dca748d42080e1ec6cc3da658b80a01eeb5
Chirag U <1078618+chiragu@users.noreply.github.com>
2020-05-28 14:57:20 -0400

    Added forms and CRUD functionality

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 
@@ -18,3 +19,13 @@ class Post(models.Model):
     # to string for admin interface/tests
     def __str__(self):
         return self.title
+
+
+    # absolute url to the model (we set it as the details page)
+    def get_absolute_url(self):
+
+        # reverse provides reference to object by URL template name
+        return reverse('post_detail', kwargs={'pk': self.pk})
+
+        # alternatively could write (kwargs for dictionary, args for list. id and pk are interchangeable)
+        # return reverse('post_detail', args=[str(self.id)])
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
 
-# import List View (for main page), Detail View (for detail pages)
-from django.views.generic import ListView, DetailView
+# import List View (for main page), Detail View (for detail pages), CreateView for new post form,
+# UpdateView for editing post, deleteview for deletion
+from django.views.generic import (
+    ListView, 
+    DetailView, 
+    CreateView, 
+    UpdateView, 
+    DeleteView,
+ )
 
 # import the models
 from .models import Post
+
+# import reverse lazy (waits for redirect until post deleted as opposed to reverse)
+from django.urls import reverse_lazy
 
 # Create your views here.
 
@@ -29,6 +39,42 @@ class BlogDetailView(DetailView):
 
     # set the template name
     template_name = 'blog/post_detail.html'
+
+# class based view for new blog post form page
+class BlogCreateView(CreateView):
+    
+    # set the model to Post
+    model = Post
+
+    # set the template name
+    template_name = 'blog/post_new.html'
+
+    # set the fields of the form
+    fields = ['title', 'author', 'body']
+
+# class based view for update blog post form page
+class BlogUpdateView(UpdateView):
+    
+    # set the model to Post
+    model = Post
+
+    # set the template name
+    template_name = 'blog/post_edit.html'
+
+    # update the fields of the form
+    fields = ['title', 'body']
+
+# class based view for delete blog post confirmation page
+class BlogDeleteView(DeleteView):
+    
+    # set the model to Post
+    model = Post
+
+    # set the template name
+    template_name = 'blog/post_delete.html'
+
+    # on success, redirect home
+    success_url = reverse_lazy('home')
 
 
 
